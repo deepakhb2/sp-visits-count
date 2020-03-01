5f4887cd7d665d08ac2e23ef9e0fa0a41bfcff51
Deepak HB <deepakhb2@gmail.com>
2020-03-01 14:00:28 +0000

    Pass the processor spec by using dependency injeciton.

--- ruby_app/lib/processor.rb
+++ ruby_app/lib/processor.rb
@@ -1,28 +1,16 @@
 # frozen_string_literal: true
 
 require_relative 'utils/desc_sort'
-require_relative 'view_count/page_view_count'
-require_relative 'view_count/unique_page_view_count'
 
 class Processor
-  def initialize(views, sort, output)
-    @views = views
-    @sort = sort
+  def initialize(page_counts, output)
+    @page_counts = page_counts
     @output = output
   end
 
   def print
-    page_counts.each do |page_count|
+    @page_counts.each do |page_count|
       @output.print(page_count)
     end
-  end
-
-  private
-
-  def page_counts
-    @page_counts ||= [
-      ViewCount::PageViewCount.new(@views, @sort),
-      ViewCount::UniquePageViewCount.new(@views, @sort)
-    ]
   end
 end
