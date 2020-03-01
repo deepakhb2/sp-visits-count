8dad253fc7b1be763d3eafeef3ac2f76f87eab47
Deepak HB <deepakhb2@gmail.com>
2020-03-01 13:55:16 +0000

    Update the testcase to inject page count object to processor.

--- ruby_app/spec/lib/processor_spec.rb
+++ ruby_app/spec/lib/processor_spec.rb
@@ -6,13 +6,18 @@ require_relative '../../lib/processor'
 
 describe Processor do
   let(:views) { YAML.load_file('spec/fixtures/lib/processor.yaml') }
-  let(:sort) { double('DescSort') }
   let(:output) { double('Console') }
+  let(:view_counts) {
+    [
+      double('ViewCount::UniquePageViewCount'),
+      double('ViewCount::PageViewCount')
+    ]
+  }
 
   it 'should print counts on page' do
     expect(output).to receive(:print)
     expect(output).to receive(:print)
-    subject = described_class.new(views, sort, output)
+    subject = described_class.new(view_counts, output)
     subject.print
   end
 end
